1612f205c5d241a71426a1da4f93d2381c987a13
Ubuntu <ubuntu@ip-10-101-0-81.ap-southeast-3.compute.internal>
2024-09-11 04:23:13 +0000

    Replacing template into the developmen code.

--- src/LazadaIDReviews/pipeline/stage_02_preprocessing.py
+++ src/LazadaIDReviews/pipeline/stage_02_preprocessing.py
@@ -1,5 +1,6 @@
+import os
 from LazadaIDReviews.config.configuration import ConfigurationManager
-from LazadaIDReviews.components.preprocessing import Preprocessing
+from LazadaIDReviews.components.preprocessing import DumpData, Preprocessing
 from LazadaIDReviews import logger
 
 STAGE_NAME = "Preprocessing"
@@ -8,19 +9,36 @@ class PreprocessingPipeline:
     def __init__(self):
         pass
 
-    def pipeline(self):
-        config = ConfigurationManager()
-        preprocessing_config = config.get_preprocessing_config()
-        preprocessing = Preprocessing(config=preprocessing_config)
-        preprocessing.preprocess_data()
+    
+    def dump_data_pipeline(self):
+        try:
+            config = ConfigurationManager()
+            dump_data_config = config.get_dump_data_config()
+            data_ingestion = DumpData(config=dump_data_config)
+            data_ingestion.dump_data()
+        except Exception as e:
+            logger.error(e)
+            raise e
+
+    def preprocessing_pipeline(self):
+        try:
+            config = ConfigurationManager()
+            preprocessing_config = config.get_preprocessing_data_config()
+            preprocessing = Preprocessing(config=preprocessing_config)
+            preprocessing.vectorize_data()
+        except Exception as e:
+            logger.error(e)
+            raise e
 
 if __name__ == '__main__':
     try:
+        os.chdir("/home/ubuntu/learning/mlops/pacmann/lazada-id-reviews")
         logger.info(f"\n\n")
         logger.info(f">>>>>>> Stage {STAGE_NAME} Started <<<<<<<")
         
         obj = PreprocessingPipeline()
-        obj.pipeline()
+        obj.dump_data_pipeline()
+        obj.preprocessing_pipeline()
         
         logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
     except Exception as e:
